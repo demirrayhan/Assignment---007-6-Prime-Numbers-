17834157836121a48473f5f20c3648d26267c526
demirrayhan <76075361+demirrayhan@users.noreply.github.com>
2021-02-07 01:34:59 -0500

    Add files via upload

---
+++ python_assignment.py
@@ -0,0 +1,9 @@
+n = int(input("please enter a number : "))
+prime_number=[]
+for i in range(2,n+1):
+    for x in range(2,i):
+        if i%x==0:
+            break
+    else:
+        prime_number.append(i)
+print(prime_number)
